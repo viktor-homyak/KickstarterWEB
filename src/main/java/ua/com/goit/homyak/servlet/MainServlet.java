dd0b4c3238599d01b1cd7181527c99e9de2fc810
viktor-homyak <h.vitya56@gmail.com>
2015-08-19 23:41:27 +0300

    scratch

--- src/main/java/ua/com/goit/homyak/servlet/MainServlet.java
+++ src/main/java/ua/com/goit/homyak/servlet/MainServlet.java
@@ -1,20 +1,13 @@
 package ua.com.goit.homyak.servlet;
 
 import ua.com.goit.homyak.QuoteGenerator;
-import ua.com.goit.homyak.dao.CategoryDAO;
 import ua.com.goit.homyak.dao.DAOFactory;
-import ua.com.goit.homyak.dao.PGConnectionPool;
 import ua.com.goit.homyak.mvc.model.CategoryModel;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -22,20 +15,20 @@ import java.util.ArrayList;
  * Created by Viktor on 18.08.2015.
  */
 public class MainServlet extends HttpServlet {
-
+private DAOFactory daoFactory;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String quote = new QuoteGenerator().getQuote();
         req.setAttribute("quote", quote);
-             CategoryModel category = (CategoryModel) getDaoFactory().getCategoryDAO();
-        req.setAttribute("category",category);
+        ArrayList<CategoryModel> category =  daoFactory.getCategoryDAO().getCategories();
+        req.setAttribute("category", category);
         req.getRequestDispatcher("main.jsp").forward(req, resp);
     }
 
 
-    private DAOFactory getDaoFactory() {
-        ServletContext servletContext = getServletContext();
+    @Override
+    public void init() throws ServletException {
+        daoFactory = DAOFactory.getDAOFactory(DAOFactory.POSTGRESQL);
 
-        return (DAOFactory) servletContext.getAttribute("daoFactory");
     }
 }
--- src/main/webapp/main.jsp
+++ src/main/webapp/main.jsp
@@ -1,24 +1,17 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Viktor
-  Date: 18.08.2015
-  Time: 19:05
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Catalog of categories</title>
+    <title>Kickstarter</title>
 </head>
 <body>
-<h2><c:out value="${quote}"/></h2>
-<h3>Now, you can choose one of categories, you prefer:</h3>
+<h1><c:out value="${quote}"/></h1>
+<br>
+<h2>Categories:</h2>
 <ol>
-<h4><c:forEach var="category" items="&{category}">
-    <li><a href="projects?categoryId=${category.id}"><c:out value="${category.name}"/></a></li>
-</c:forEach> </h4>
+    <c:forEach var="category" items="${categories}">
+        <li><a href="projects?categoryId=${category.id}"><c:out value="${category.name}"/></a></li>
+    </c:forEach>
 </ol>
 </body>
 </html>
